a9e75aad535689f5a5b5923972cb20e1a20bd54f
duolonW <125333834+duolonW@users.noreply.github.com>
2026-02-21 16:36:20 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -11,4 +11,5 @@
 #
 
 ### 添加第三方订阅源
-sed -i '$a src-git-full small https://github.com/kenzok8/small-package' feeds.conf.default
+sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+sed -i '2i src-git small https://github.com/kenzok8/small' feeds.conf.defaul
